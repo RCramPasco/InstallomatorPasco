199b60589616f37bd09f6f85ecff8c35cdbcd5ba
RCramPasco <rcram@pasco.k12.fl.us>
2023-06-02 14:19:26 -0400

    Create flsecurebrowser15.sh
    FLSecureBrowser Testing platform for up to Monterey

---
+++ fragments/labels/flsecurebrowser15.sh
@@ -0,0 +1,7 @@
+flsecurebrowser15)
+    name="FLSecureBrowser"
+    type="dmg"
+    downloadURL="https://sb.portal.cambiumast.com/geturls?clientName=florida&operatingSystem=macOS&browserVersion=15.0"
+    appNewVersion="15.0"
+    expectedTeamID="FRFWLA39JY"
+    ;;
