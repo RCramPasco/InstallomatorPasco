9aa66d70ba84d0a3f8f36492ab7024f12f4f85d0
RCramPasco <rcram@pasco.k12.fl.us>
2023-06-02 14:18:19 -0400

    Create flsecurebrowser12_5.sh
    Older version of the FLSecureBrowser testing platform

---
+++ fragments/labels/flsecurebrowser12_5.sh
@@ -0,0 +1,7 @@
+flsecurebrowser12_5)
+    name="FLSecureBrowser"
+    type="dmg"
+    downloadURL="https://files.portal.cambiumast.com/florida/SecureBrowsers/SB2020/FLSecureBrowser12.5-2020-07-08-signed.dmg"
+    appNewVersion="12.5"
+    expectedTeamID="FRFWLA39JY"
+    ;;
