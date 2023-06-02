aa4ba2bd0e4eb75c3b56d659237fc07b9cc051ce
RCramPasco <rcram@pasco.k12.fl.us>
2023-06-02 14:20:08 -0400

    Create flsecurebrowser15_5.sh
    FLSecureBrowser for Ventura

---
+++ fragments/labels/flsecurebrowser15_5.sh
@@ -0,0 +1,7 @@
+flsecurebrowser15_5)
+    name="FLSecureBrowser"
+    type="dmg"
+    downloadURL="https://sb.portal.cambiumast.com/geturls?clientName=florida&operatingSystem=macOS&browserVersion=15.5"
+    appNewVersion="15.5"
+    expectedTeamID="FRFWLA39JY"
+    ;;
