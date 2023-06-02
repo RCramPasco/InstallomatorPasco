5ddc3c660b81f22691b2ffb4f96dd676e677827e
RCramPasco <rcram@pasco.k12.fl.us>
2023-06-02 14:20:46 -0400

    Create nweasecurebrowser.sh
    NWEA Secure Testing browser

---
+++ fragments/labels/nweasecurebrowser.sh
@@ -0,0 +1,7 @@
+nweasecurebrowser)
+    name="NWEA Secure Testing Browser"
+    type="dmg"
+    downloadURL="https://teach.mapnwea.org/contents/partner/Lockdown%20Browser.dmg"
+    appNewVersion="5.5.2.3"
+    expectedTeamID="SRTXZJ7SQ3"
+    ;;
